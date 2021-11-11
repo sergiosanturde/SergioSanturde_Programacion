70b47a9f23af4a7823e2a8f7b5f007135f9dacfb
sergiosanturde <72124215+sergiosanturde@users.noreply.github.com>
2021-11-11 11:30:34 +0100

    update

---
+++ T4/Codigos2122/T4_Arrays/src/Arrays.java
@@ -0,0 +1,20 @@
+public class Arrays {
+
+    public static void main(String[] args) {
+        //array de numeros
+        int[] numeros = new int[10];
+        int[] numeroDos = new int[]{1,2,3,4,5,6,7,8,9,10};
+
+        boolean[] aciertos = new boolean[5];
+        String[] palabras = new String[3];
+
+        double[] decimales = new double[2];
+        int longitudArray = numeroDos.length;
+        //System.out.println(longitudArray);
+        System.out.println(numeroDos[9]);
+
+        for (int i = 0; i < numeroDos.length; i++) {
+            System.out.printf(numeroDos,[i];
+        }
+    }
+}
