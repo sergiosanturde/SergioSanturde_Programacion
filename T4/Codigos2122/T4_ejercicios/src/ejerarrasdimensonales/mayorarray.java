ebe7c6dd796dee000bd410e4670fdfa222cfbd78
sergiosanturde <72124215+sergiosanturde@users.noreply.github.com>
2021-11-18 10:26:32 +0100

    update

---
+++ T4/Codigos2122/T4_ejercicios/src/ejerarrasdimensonales/mayorarray.java
@@ -0,0 +1,40 @@
+package ejerarrasdimensonales;
+
+
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class mayorarray {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int longitud = 0;
+        int[] numeros;
+        int max = -9999, min = 9999;
+        System.out.println("De que longitud quieres crear el array?");
+        longitud = teclado.nextInt();
+        numeros = new int[longitud];
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Introduce un numero");
+            int numeroInto = teclado.nextInt();
+
+            numeros[i] = numeroInto;
+        }
+
+        System.out.println("Desordenados");
+
+        for (int item : numeros) {
+            System.out.println();
+        }
+
+        System.out.println("Ordenados");
+
+        Arrays.sort(numeros);
+
+
+
+        }
+    }
+
