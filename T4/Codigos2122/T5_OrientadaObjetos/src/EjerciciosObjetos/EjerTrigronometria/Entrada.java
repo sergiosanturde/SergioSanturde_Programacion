de9653f295f12ea69e3b10877c46cd2f962f8bcf
sergiosanturde <72124215+sergiosanturde@users.noreply.github.com>
2022-01-21 12:08:24 +0100

    update

--- T4/Codigos2122/T5_OrientadaObjetos/src/EjerciciosObjetos/EjerTrigronometria/Entrada.java
+++ T4/Codigos2122/T5_OrientadaObjetos/src/EjerciciosObjetos/EjerTrigronometria/Entrada.java
@@ -1,7 +1,10 @@
 package EjerciciosObjetos.EjerTrigronometria;
 
+import java.util.Scanner;
+
 public class Entrada {
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
 
         Triangulo triangulo1 = new Triangulo(5,5);
 
@@ -16,10 +19,31 @@ public class Entrada {
         System.out.println("El perimetro del Cuadrado es: " + cuadrado.calcularPerimetro());
         //mod
 
-        System.out.println("¿Qué quieres selccionar?");
-        System.out.println("");
-        System.out.println("");
+        System.out.println("¿Qué quieres seleccionar?");
+        System.out.println("1.- Trabajar con triángulos");
+        System.out.println("2.- Trabajar con circulos");
+        System.out.println("3.-Trabajar con cuadrados");
+        System.out.println("4. Salir");
 
+        System.out.println("Escribe una de las opciones");
+        int opcion = teclado.nextInt();
+
+        switch (opcion){
+
+            case 1:
+                System.out.println("Introduce una base");
+                int base = teclado.nextInt();
+                System.out.println("Introduce una altura");
+                int altura = teclado.nextInt();
+                System.out.println("¿Qué operacion quieres hacer?");
+                System.out.println("Calcular area");
+                System.out.println("Mostrar datos");
+                int opcion1 = teclado.nextInt();
+                    switch (opcion1){
+                        case 1:
+
+                    }
+        }
 
 
 
